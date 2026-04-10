462d1b955d546de6b90883270113b0cbd11413c7
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-04-10 16:57:28 -0500

    import dotenv in server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+import "dotenv/config.js";
 import express from 'express';
 const app = express();
+const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
     res.send('hello world');
 });
-app.listen(3000);
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}.`);
+});
