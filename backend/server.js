b5fb8079fc2e530463fd6d72c79b9986b34021db
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-04-11 13:33:41 -0500

    connect supabase to server

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,8 @@ import "dotenv/config.js";
 import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/health', (req, res) => {
+    res.send({ 'status': 'ok' });
 });
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}.`);
--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,26 @@
 import "dotenv/config.js";
-import express from 'express';
+import express from "express";
+import sql from "./db.js";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get("/health", (req, res) => {
+    res.json({ status: "ok"});
 });
 
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
-});
+async function startServer() {
+    try {
+        const result = await sql`SELECT NOW()`;
+        console.log("Database connected:", result);
+
+        app.listen(PORT, () => {
+            console.log(`Listening on port ${PORT}.`);
+        });
+    } catch (error) {
+        console.log("Failed to connect to database:", error);
+        process.exit(1);
+    }
+}   
+
+startServer();
