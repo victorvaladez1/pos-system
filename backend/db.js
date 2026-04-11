16b50697758a4015d669620f3b3ac1bedaaa356d
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-04-11 14:02:23 -0500

    add /time endpoint to server

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-import 'dotenv/config';
-import postgres from 'postgres';
-if (!process.env.DATABASE_URL) {
-    throw new Error("DATABASE_URL is not defined in the enviroment");
-}
-const sql = postgres(process.env.DATABASE_URL);
-export default sql;
--- backend/server.ts
+++ backend/server.ts
@@ -9,13 +9,23 @@ app.get("/health", (req, res) => {
     res.json({ status: "ok"});
 });
 
+app.get("/time", async (req, res) => {
+    try {
+        const result = await sql`SELECT NOW()`;
+        res.json({time: result[0].now});
+    } catch (error) {
+        console.error("Failed to fetch time:", error);
+        res.status(500).json({ error: "Failed to fetch time" });
+    }
+});
+
 async function startServer() {
     try {
         const result = await sql`SELECT NOW()`;
         console.log("Database connected:", result);
 
         app.listen(PORT, () => {
-            console.log(`Listening on port ${PORT}.`);
+            console.log(`Listening on port ${PORT}!`);
         });
     } catch (error) {
         console.log("Failed to connect to database:", error);
