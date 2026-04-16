1fd553aaf06de4e2efee0909eb36719142855899
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-04-15 21:07:26 -0500

    create api endpoints for PATCH /categories/:id and DELETE /categories/:id

--- backend/src/controllers/categoriesController.ts
+++ backend/src/controllers/categoriesController.ts
@@ -1,9 +1,12 @@
 import { Request, Response } from "express";
+import { validate as isUuid } from "uuid";
 import { 
     createCategoryRow,
     getAllCategoryRows,
+    deleteCategoryRowById,
+    updateCategoryNameById,
     getCategoryRowByName
-} from "../services/categoryService.js"
+} from "../services/categoryService.js";
 
 export async function createCategory(req: Request, res: Response) {
     const { name } = req.body ?? {};
@@ -32,11 +35,40 @@ export async function getAllCategories(req: Request, res: Response) {
     }
 }
 
-export function updateCategory(req: Request, res: Response) {
-    res.json({ msg: "Update existing category"});
+export async function updateCategory(req: Request, res: Response) {
+    const { id } = req.params ?? {};
+
+    if (!isUuid(id) || Array.isArray(id)) {
+        return res.status(400).json({ error: "Invalid UUID" });
+    }
+
+    const { name } = req.body ?? {};
+
+    if (!name || typeof name !== "string" || name.trim() === "") {
+        return res.status(400).json({ error: "Category name is required." });
+    }
+
+    try {
+        const updatedRow = await updateCategoryNameById(id, name);
+        return res.json({ updatedRow });
+    } catch (error) {
+        console.error("Failed to update category row name column in db.");
+        res.status(500).json({ error: "Failed to update category row name column in db." });
+    }
 }
 
-export function deleteCategory(req: Request, res: Response) {
-    res.json({ msg: "Delete existing category"});
-}
+export async function deleteCategory(req: Request, res: Response) {
+    const { id } = req.params ?? {};
 
+    if (!isUuid(id) || Array.isArray(id)) {
+        return res.status(400).json({ error: "Invalid UUID" });
+    }
+
+    try {
+        const deletedRow = await deleteCategoryRowById(id);
+        res.json({ deletedRow });
+    } catch (error) {
+        console.error("Failed to delete category row from db:", error);
+        res.status(500).json({ error: "Failed to delete category row from db." });
+    }
+}
--- backend/src/services/categoryService.ts
+++ backend/src/services/categoryService.ts
@@ -10,6 +10,16 @@ export async function getAllCategoryRows() {
     return result;
 };
 
+export async function updateCategoryNameById(categoryId: string, categoryName: string) {
+    const result = await sql`UPDATE categories SET name = ${categoryName} WHERE id = ${categoryId}`;
+    return result;
+}
+
+export async function deleteCategoryRowById(categoryId: string) {
+    const result = await sql`DELETE FROM categories WHERE id = ${categoryId}`;
+    return result;
+}
+
 export async function getCategoryRowByName(categoryName: string) {
     const result = await sql`SELECT * FROM categories WHERE name = ${categoryName} LIMIT 1`;
     return result;
