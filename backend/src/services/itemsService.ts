5df60d204db462508ef095874af4605e384ce90b
Victor Manuel Valadez <56981285+victorvaladez1@users.noreply.github.com>
2026-04-18 11:28:13 -0500

    create api endpoints for PATCH /items/:id and DELETE /items/:id

--- backend/src/services/itemsService.ts
+++ backend/src/services/itemsService.ts
@@ -25,7 +25,22 @@ export async function getAllItemRows() {
     return result;
 };
 
+export async function getItemRowById(itemId: string) {
+    const result = await sql`SELECT * FROM items WHERE id = ${itemId} LIMIT 1`;
+    return result;
+}
+
 export async function getItemRowByName(name: string) {
     const result = await sql`SELECT * FROM items WHERE name = ${name} LIMIT 1`;
+    return result;
+}
+
+export async function updateItemRowById(itemId: string, name: string, description: string, price_in_cents: number, category_id: string, is_active: boolean) {
+    const result = await sql`UPDATE items SET name = ${name}, description = ${description}, price_in_cents = ${price_in_cents}, category_id = ${category_id}, is_active = ${is_active} WHERE id = ${itemId} RETURNING id`; 
+    return result;
+}
+
+export async function deleteItemRowById(itemId: string) {
+    const result = await sql`DELETE FROM items WHERE id = ${itemId}`;
     return result;
 }
