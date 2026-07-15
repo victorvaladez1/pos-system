f9535430048d40f98e9d5b2cabe815753f0484a2
Victor Valadez <vvaladez315@gmail.com>
2026-07-15 17:27:05 -0500

    Implement order item update and delete API

--- backend/src/controllers/orderItems.controller.ts
+++ backend/src/controllers/orderItems.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { validate as isUuid } from "uuid";
-import { CreateOrderItemRequest, isOrderItemStatus } from "../types/orderItem.type.js";
+import { CreateOrderItemRequest, isOrderItemStatus, UpdateOrderItemRequest } from "../types/orderitem.type.js";
 import {
     createOrderItemRow,
-    getOrderItemRows
+    getOrderItemRows,
+    updateOrderItemRowById,
+    deleteOrderItemRowById
 } from "../services/orderItems.service.js";
 
 export const createOrderItem = async (req: Request, res: Response) => {
@@ -67,10 +69,99 @@ export const getOrderItem = async (req: Request, res: Response) => {
     }
 };
 
-export const updateOrderItem = (req: Request, res: Response) => {
-    return res.status(200).json({ "msg": "Order Item updated." });
+export const updateOrderItem = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { quantity, unit_price_in_cents, notes, order_item_status } = req.body ?? {};
+
+    if (!id || typeof id !== "string" || !isUuid(id) || Array.isArray(id)) {
+        return res.status(400).json({ error: "Order item id must be a valid UUID."});
+    }
+
+    if (quantity !== undefined && (typeof quantity !== "number" || !Number.isInteger(quantity) || quantity < 1)) {
+        return res.status(400).json({ error: "Quantity must be positive integer." });
+    }
+
+    if (
+        unit_price_in_cents !== undefined &&
+        (
+            typeof unit_price_in_cents !== "number" ||
+            !Number.isInteger(unit_price_in_cents) ||
+            unit_price_in_cents < 0
+        )
+    ) {
+        return res.status(400).json({ error: "Price in cents must be non-negative integer." });
+    }
+
+    if (
+        notes !== undefined &&
+        notes !== null &&
+        (
+            typeof notes !== "string" ||
+            Array.isArray(notes) ||
+            notes.trim() === ""
+        )
+    ) {
+        return res.status(400).json({ error: "Notes must be non-empty string." });
+    }
+
+    if (order_item_status !== undefined && (typeof order_item_status !== "string" || !isOrderItemStatus(order_item_status))) {
+        return res.status(400).json({ error: "Order item status must be valid." });
+    }
+
+    const fieldsToUpdate: UpdateOrderItemRequest = {};
+
+    if (quantity !== undefined) {
+        fieldsToUpdate.quantity = quantity;
+    }
+
+    if (unit_price_in_cents !== undefined) {
+        fieldsToUpdate.unit_price_in_cents = unit_price_in_cents;
+    }
+
+    if (notes !== undefined) {
+        fieldsToUpdate.notes = notes === null ? null : notes.trim();
+    }
+
+    if (order_item_status !== undefined) {
+        fieldsToUpdate.order_item_status = order_item_status;
+    }
+
+    if (Object.keys(fieldsToUpdate).length === 0) {
+        return res.status(400).json({ error: "No valid fields provided to update." });
+    }
+
+    try {
+        const orderItem = await updateOrderItemRowById(id, fieldsToUpdate);
+
+        if (!orderItem) {
+            return res.status(404).json({ error: "Order item not found." });
+        }
+        
+        return res.status(200).json({ orderItem });
+    } catch (error: any) {
+        console.error("Falied to update item.", error);
+        return res.status(500).json({ error: "Failed to update order item." });
+    }
 };
 
-export const deleteOrderItem = (req: Request, res: Response) => {
-    return res.status(200).json({ "msg": "Order Item deleted." });
+export const deleteOrderItem = async (req: Request, res: Response) => {
+    const { id } = req.params;
+
+    if (!id || typeof id !== "string" || !isUuid(id) || Array.isArray(id)) {
+        return res.status(400).json({ error: "Order item id must be a vlid UUID." });
+    }
+
+    try {
+        const deletedOrderItem = await deleteOrderItemRowById(id);
+
+        if (!deletedOrderItem) {
+            return res.status(404).json({ error: "Order item not found." });
+        }
+
+        return res.sendStatus(204);
+    } catch (error: any) {
+        console.error("Failed to delete order item.", error);
+        return res.status(500).json({ error: "Failed to delete order item." });
+    }
+
 };
--- backend/src/services/orderItems.service.ts
+++ backend/src/services/orderItems.service.ts
@@ -3,7 +3,7 @@ import {
     OrderItem,
     CreateOrderItemRequest,
     UpdateOrderItemRequest
-} from "../types/orderItem.type.js";
+} from "../types/orderitem.type.js"
 
 export const createOrderItemRow = async (fields: CreateOrderItemRequest): Promise<OrderItem> => {
     const result = await sql<OrderItem[]>`INSERT INTO order_items (order_id, item_id, quantity, unit_price_in_cents, notes, order_item_status) 
@@ -15,3 +15,20 @@ export const getOrderItemRows = async (): Promise<OrderItem[]> => {
     const result = await sql<OrderItem[]>`SELECT * FROM order_items ORDER BY created_at DESC`;
     return result;
 };
+
+export const updateOrderItemRowById = async (
+    orderItemId: string,
+    fieldsToUpdate: UpdateOrderItemRequest
+) : Promise<OrderItem | undefined> => {
+    const result = await sql<OrderItem[]>`UPDATE order_items SET ${sql(fieldsToUpdate)}, updated_at = NOW() WHERE id = ${orderItemId} RETURNING *`;
+
+    return result[0];
+};
+
+export const deleteOrderItemRowById = async (
+    orderItemId: string
+) : Promise<OrderItem | undefined> => {
+    const result = await sql<OrderItem[]>`DELETE FROM order_items WHERE id = ${orderItemId} RETURNING *`;
+
+    return result[0];
+}
